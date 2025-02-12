438c47e99ee26545942d50fb770b3b2542ff2df0
BaptisteFrancois <baptistefrancois51@gmail.com>
2025-02-12 16:03:23 -0500

    initial commit

---
+++ CST_PlottingTools/CRF_lineplot.py
@@ -0,0 +1,93 @@
+
+
+import matplotlib.pyplot as plt
+
+
+def TwoVarLineplot(array, x_axis, z_dim, title, ylabel, xlabel, colors, caption_labels=None,
+                            threshold=None, color_threshold='red', label_threshold='Target', 
+                            linewidth=2, figsize=(8, 5), title_fontsize=14, label_fontsize=12, grid=True, 
+                            facecolor='whitesmoke', legend_facecolor='whitesmoke', savepath=None, 
+                            show=False):
+
+    """
+        - array: numpy array. The array to be plotted. The dimensions must be (x_axis, z_dim)
+        - x_axis: list. List of the xticks to be plotted
+        - z_dim: list. List of the 'array' indices on the y-axis to be plotted
+        - title: str. Title of the plot
+        - ylabel: str. Label of the y-axis
+        - xlabel: str. Label of the x-axis
+        - caption_labels: str or None. Label of the captions. This is the label of the lines in the plot that
+                        represent the z_dim. If None, no label is plotted
+        - colors: list. List of colors to be used in the plot
+        - threshold: float. Value to be plotted as a horizontal line. Default is None, which results
+                        in no horizontal line being plotted
+        - color_threshold: str. Color of the threshold line. Default is 'red'
+        - label_threshold: str. Label of the threshold line. Default is 'Target'
+        - linewidth: int. Width of the lines. Default is 2
+        - figsize: tuple. Size of the figure. Default is (12, 6).
+        - title_fontsize: int. Fontsize of the title. Default is 14.
+        - label_fontsize: int. Fontsize of the labels. Default is 12.
+        - grid: bool. If True, the grid is plotted. Default is True
+        - facecolor: str. Background color of the plot. Default is 'whitesmoke'
+        - legend_facecolor: str. Background color of the legend. Default is 'whitesmoke'
+        - savepath: str. Path to save the plot. Default is None, which would result in the plot 
+                    being displayed but not saved
+        - show: bool. If True, the plot is displayed. Default is True
+
+    """
+
+    try:
+        len(x_axis) == array.shape[0]
+    except:
+        raise ValueError('The x_axis must have the same length as the array')
+    
+    try:
+        len(colors) == len(z_dim)
+    except: 
+        raise ValueError('The number of colors must be equal to the number of z_dim')
+    
+    try:
+        max(z_dim) < array.shape[1]
+    except:
+        raise ValueError('The maximum value of z_dim must be less than the number of dimensions of the array')
+
+
+    fig, ax = plt.subplots(figsize=figsize)
+
+    for z in z_dim:
+        if caption_labels:
+            label = caption_labels[z]
+        else:
+            label = None
+        ax.plot(x_axis, array[:,z], color=colors[z], label=label, lw=linewidth)
+
+    if threshold:
+        ax.axhline(threshold, color=color_threshold, linestyle='--', label=label_threshold)
+
+    ax.set_title(title, fontsize=title_fontsize)
+    ax.set_ylabel(ylabel, fontsize=label_fontsize)
+    ax.set_xlabel(xlabel, fontsize=label_fontsize)
+    
+    if facecolor:
+        ax.set_facecolor(facecolor)
+
+    if caption_labels:
+        handles, labels = ax.get_legend_handles_labels()
+        legend = plt.figlegend(handles, labels, columnspacing=0.5, handletextpad=0.5, 
+                               loc='lower center', ncol=len(z_dim), bbox_to_anchor=(0.5,-0.004),
+                               fontsize=label_fontsize)
+        if legend_facecolor:
+            legend.get_frame().set_facecolor(legend_facecolor)
+        
+    if grid:
+        ax.grid()
+
+    plt.tight_layout()
+    plt.subplots_adjust(bottom=0.18)
+    
+    if show:
+        plt.show()
+
+    if savepath:
+        fig.savefig(savepath)
+    plt.close()
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CST_PlottingTools',
+    version='0.0.1',
+    description='A set of tools for plotting Climate Stress Test results',
+    url='https://github.com/BaptisteFrancois/CST_PlottingTools.git',
+    author='BaptisteFrancois',
+    author_email='BaptisteFrancois51@gmail.com',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'seaborn',
+    ]
+)
