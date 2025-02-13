e031a59e66b939d8816ef6ab9063b9e36bbbbcef
BaptisteFrancois <baptistefrancois51@gmail.com>
2025-02-13 15:30:51 -0500

    update CRF_lineplot

--- CST_PlottingTools/CRF_lineplot.py
+++ CST_PlottingTools/CRF_lineplot.py
@@ -3,24 +3,26 @@
 import matplotlib.pyplot as plt
 
 
-def TwoVarLineplot(array, x_axis, z_dim, title, ylabel, xlabel, colors, caption_labels=None,
-                            threshold=None, color_threshold='red', label_threshold='Target', 
-                            linewidth=2, figsize=(8, 5), title_fontsize=14, label_fontsize=12, grid=True, 
-                            facecolor='whitesmoke', legend_facecolor='whitesmoke', savepath=None, 
-                            show=False):
+def TwoVarLineplot(
+        array, x_axis, z_dim, title='', ylabel='', xlabel='', colors='k', caption_labels=None,
+        threshold=None, color_threshold='red', label_threshold='Target', linewidth=2, figsize=(8, 5), 
+        title_fontsize=14, label_fontsize=12, grid=True, facecolor='whitesmoke', 
+        legend_facecolor='whitesmoke', subplot_adjust=(None, None, 0.18, None), savepath=None, 
+        show=False):
 
-    """
+    """ Create line plots for two-dimensional arrays
         - array: numpy array. The array to be plotted. The dimensions must be (x_axis, z_dim)
-        - x_axis: list. List of the xticks to be plotted
-        - z_dim: list. List of the 'array' indices on the y-axis to be plotted
-        - title: str. Title of the plot
-        - ylabel: str. Label of the y-axis
-        - xlabel: str. Label of the x-axis
-        - caption_labels: str or None. Label of the captions. This is the label of the lines in the plot that
-                        represent the z_dim. If None, no label is plotted
-        - colors: list. List of colors to be used in the plot
+        - x_axis: list or one-dimensional numpy array. List of the xticks to be plotted
+        - z_dim: list or one-dimensional numpy array. List of the 'array' indices on the y-axis to be plotted
+        - title: str. Title of the plot. Default is an empty string.
+        - ylabel: str. Label of the y-axis. Default is an empty string.
+        - xlabel: str. Label of the x-axis. Default is an empty string.
+        - caption_labels: str or None. Label of the captions. This is the label of the lines in the 
+                          plot that represent the z_dim. If None, no label is plotted
+        - colors: str or list. List of colors to be used in the plot. If a str, the same color will 
+                  be applied to all lines. Default value is 'k' (black).
         - threshold: float. Value to be plotted as a horizontal line. Default is None, which results
-                        in no horizontal line being plotted
+                     in no horizontal line being plotted
         - color_threshold: str. Color of the threshold line. Default is 'red'
         - label_threshold: str. Label of the threshold line. Default is 'Target'
         - linewidth: int. Width of the lines. Default is 2
@@ -30,10 +32,11 @@ def TwoVarLineplot(array, x_axis, z_dim, title, ylabel, xlabel, colors, caption_
         - grid: bool. If True, the grid is plotted. Default is True
         - facecolor: str. Background color of the plot. Default is 'whitesmoke'
         - legend_facecolor: str. Background color of the legend. Default is 'whitesmoke'
+        - subplot_adjust: tuple. Adjust the subplot (top, right, bottom, left).
+                          Default is (None, None, 0.18, None) 
         - savepath: str. Path to save the plot. Default is None, which would result in the plot 
                     being displayed but not saved
         - show: bool. If True, the plot is displayed. Default is True
-
     """
 
     try:
@@ -83,7 +86,10 @@ def TwoVarLineplot(array, x_axis, z_dim, title, ylabel, xlabel, colors, caption_
         ax.grid()
 
     plt.tight_layout()
-    plt.subplots_adjust(bottom=0.18)
+    plt.subplots_adjust(top=subplot_adjust[0], 
+                        right=subplot_adjust[1], 
+                        bottom=subplot_adjust[2], 
+                        left=subplot_adjust[3])
     
     if show:
         plt.show()
